7029906add784d222524fd6a2fe0b73f29540f34
Mykhailo Pichka <dusttailtale@gmail.com>
2024-06-28 18:13:44 +0300

    Migration to React

--- src/main.ts
+++
@@ -1,5 +0,0 @@
-import '@/assets/scss/globals.scss';
-import { createApp } from 'vue';
-import App from './App.vue';
-
-createApp(App).mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,7 @@
-import vue from '@vitejs/plugin-vue';
-import { URL, fileURLToPath } from "url";
-import { defineConfig } from 'vite';
-import svgLoader from 'vite-svg-loader';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), svgLoader({
-    svgoConfig: {
-      multipass: true
-    }
-  })],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "@/assets/scss/_variables.scss";`,
-      },
-    },
-  },
-});
+  plugins: [react()],
+})
